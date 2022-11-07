50cc17aff08f0a3aad99dcc95ff0cd62b8c66ef0
Manuel Fuchs <manuel.fuchs@softaware.at>
2022-11-07 18:04:13 +0100

    Resource Creator: Save configuration in .env file

--- src/1_resource_creator.py
+++ src/1_resource_creator.py
@@ -1,25 +1,25 @@
-import json
 import os
+import json
 import subprocess
 from typing import Union
 
 from azure.ai.ml import MLClient
 from azure.ai.ml.entities import AmlCompute, Environment, IdentityConfiguration, ManagedIdentityConfiguration
 from azure.identity import DefaultAzureCredential
-from dotenv import load_dotenv
-
-load_dotenv('../.env')
+import dotenv
 
 
 def execute_cli_command(command: str) -> Union[str, list, dict]:
     command = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                universal_newlines=True, shell=True)
-    output, error = command.communicate()
+    output, error_output = command.communicate()
 
-    if error:
-        raise RuntimeError(error)
+    if command.returncode != 0:
+        raise RuntimeError(error_output)
 
     if output.startswith('[') or output.startswith('{'):
+        color_reset_code = '\x1b[0m'
+        output = output[: -len(color_reset_code)] if output.endswith(color_reset_code) else output
         return json.loads(output)
     else:
         return output
@@ -45,67 +45,86 @@ def main():
     compute_cluster_name = 'cluster-standard-ds3-v2'
     environment_name = 'env-digit-classifier'
 
-    resource_group_name = request_resource_group_name()
-    print('\n')
+    resource_group_name, subscription_id, aml_workspace_name = request_aml_workspace_info()
 
     managed_id_principal_id, managed_id_client_id, managed_id_resource_id =\
         create_managed_identity(resource_group_name, managed_id_name)
 
-    storage_account_id = fetch_storage_account_id(resource_group_name)
+    storage_account_id, storage_account_name = fetch_storage_account_data(resource_group_name)
 
     grant_storage_account_permissions(managed_id_principal_id, storage_account_id)
 
     ml_client = MLClient(
         credential=DefaultAzureCredential(),
-        subscription_id=os.getenv('SUBSCRIPTION_ID'),
-        resource_group_name=os.getenv('RESOURCE_GROUP'),
-        workspace_name=os.getenv('AML_WORKSPACE_NAME'),
+        subscription_id=subscription_id,
+        resource_group_name=resource_group_name,
+        workspace_name=aml_workspace_name,
     )
 
     create_compute_cluster(ml_client, compute_cluster_name, managed_id_client_id, managed_id_resource_id)
     create_environment(ml_client, environment_name)
 
-    # todo: Update .env file
+    save_configuration_in_environment(subscription_id, resource_group_name, aml_workspace_name, storage_account_name,
+                                      compute_cluster_name, managed_id_client_id, managed_id_resource_id,
+                                      environment_name)
 
 
-def request_resource_group_name() -> str:
+def request_aml_workspace_info() -> (str, str, str):
     print('\n\nEnter the resource group\'s name where the Azure Machine Learning workspace is located:')
     exists = False
     while exists is False:
         # TODO: improve with multi-select
-        resource_group = input('\nResource group> ')
-        exists = resource_group in execute_cli_command('az group list --query "[].name"')
+        resource_group_name = input('Resource group> ')
+
+        resource_groups: list[dict] = execute_cli_command('az group list' +
+                                                          ' --query "[].{name:name,id:id}"')
+        exists = resource_group_name in [rg['name'] for rg in resource_groups]
         if exists is False:
             print('\n\tThis resource group does not exist, please try again')
 
-    return resource_group
+    print('')
+
+    action_text = 'Gather AML workspace info'
+    start_action(action_text)
+
+    resource_group_id: str = [rg['id'] for rg in resource_groups if rg['name'] == resource_group_name][0]
+    subscription_id = resource_group_id.split('/')[2]
+
+    output = execute_cli_command(f'az ml workspace list' +
+                                 f' --resource-group {resource_group_name}' +
+                                 f' --query "[].name"')
+    aml_workspace_name = output[0]
+
+    end_action(action_text)
+
+    return resource_group_name, subscription_id, aml_workspace_name
 
 
 def create_managed_identity(resource_group: str, managed_id_name: str) -> (str, str, str):
     action_text = 'Create user-assigned managed identity'
     start_action(action_text)
 
-    managed_id: dict = execute_cli_command(f'az identity create ' +
-                                           f'--resource-group {resource_group} ' +
-                                           f'--name {managed_id_name}')
+    managed_id: dict = execute_cli_command(f'az identity create' +
+                                           f' --resource-group {resource_group}' +
+                                           f' --name {managed_id_name}')
 
     end_action(action_text)
 
     return managed_id['principalId'], managed_id['clientId'], managed_id['id']
 
 
-def fetch_storage_account_id(resource_group_name: str) -> str:
-    action_text = 'Fetch storage account id list'
+def fetch_storage_account_data(resource_group_name: str) -> (str, str):
+    action_text = 'Fetch storage accounts in resource group'
     start_action(action_text)
 
-    storage_account_ids = execute_cli_command(f'az storage account list ' +
-                                              f'--resource-group {resource_group_name} ' +
-                                              f'--query "[].id"')
+    storage_account_ids: list[dict] = execute_cli_command('az storage account list' +
+                                                          f' --resource-group {resource_group_name}' +
+                                                          '  --query "[].{id:id,name:name}"')
 
     end_action(action_text)
 
     if len(storage_account_ids) == 1:
-        return storage_account_ids[0]
+        return storage_account_ids[0]['id'], storage_account_ids[0]['name']
     else:
         # TODO: pick single storage account id
         raise ValueError('not yet implemented!')
@@ -116,10 +135,10 @@ def grant_storage_account_permissions(managed_id_principal_id: str, storage_acco
     action_text = f'Grant user-assigned managed identity {role} on storage account'
     start_action(action_text)
 
-    _ = execute_cli_command(f'az role assignment create ' +
-                            f'--assignee "{managed_id_principal_id}" ' +
-                            f'--role "{role}" ' +
-                            f'--scope "{storage_account_id}"')
+    _ = execute_cli_command(f'az role assignment create' +
+                            f' --assignee "{managed_id_principal_id}"' +
+                            f' --role "{role}"' +
+                            f' --scope "{storage_account_id}"')
 
     end_action(action_text)
 
@@ -157,6 +176,24 @@ def create_environment(ml_client: MLClient, environment_name: str):
     )
 
     ml_client.environments.create_or_update(pipeline_job_env)
+
+    end_action(action_text)
+
+
+def save_configuration_in_environment(subscription_id, resource_group_name, aml_workspace_name, storage_account_name,
+                                      compute_cluster_name, managed_id_client_id, managed_id_resource_id,
+                                      environment_name):
+    action_text = 'Save configuration in .env file'
+    start_action(action_text)
+
+    dotenv.set_key('../.env', 'SUBSCRIPTION_ID', subscription_id)
+    dotenv.set_key('../.env', 'RESOURCE_GROUP', resource_group_name)
+    dotenv.set_key('../.env', 'AML_WORKSPACE_NAME', aml_workspace_name)
+    dotenv.set_key('../.env', 'AML_STORAGE_ACCOUNT', storage_account_name)
+    dotenv.set_key('../.env', 'COMPUTE_INSTANCE_NAME', compute_cluster_name)
+    dotenv.set_key('../.env', 'COMPUTE_IDENTITY_CLIENT_ID', managed_id_client_id)
+    dotenv.set_key('../.env', 'COMPUTE_IDENTITY_RESOURCE_ID', managed_id_resource_id)
+    dotenv.set_key('../.env', 'ENVIRONMENT_NAME', environment_name)
 
     end_action(action_text)
 
