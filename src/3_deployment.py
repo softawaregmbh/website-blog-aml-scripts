137f6625f2cbb36ee71a5c6170c131f88a6f568f
Manuel Fuchs <manuel.fuchs@softaware.at>
2022-11-09 11:28:10 +0100

    Deployment: Save deployment name in environment configuration

--- src/3_deployment.py
+++ src/3_deployment.py
@@ -10,6 +10,9 @@ dotenv.load_dotenv('../.env')
 
 
 def main():
+    endpoint_name = f'digit-endpoint-{str(uuid.uuid4())[:8]}'
+    deployment_name = 'blue'
+
     ml_client = MLClient(
         credential=DefaultAzureCredential(),
         subscription_id=os.getenv('SUBSCRIPTION_ID'),
@@ -17,15 +20,16 @@ def main():
         workspace_name=os.getenv('AML_WORKSPACE_NAME'),
     )
 
-    endpoint, endpoint_name = create_endpoint(ml_client)
+    scoring_uri = create_endpoint(ml_client, endpoint_name)
 
-    deployment = deploy_model_to_endpoint(endpoint_name, ml_client)
+    deploy_model_to_endpoint(ml_client, endpoint_name, deployment_name)
 
-    update_environment_configuration(deployment, endpoint)
+    # TODO: Retrieve primary key API token for authorization against endpoint
+
+    update_environment_configuration(scoring_uri, endpoint_name, deployment_name)
 
 
-def create_endpoint(ml_client: MLClient):
-    endpoint_name = f'digit-endpoint-{str(uuid.uuid4())[:8]}'
+def create_endpoint(ml_client: MLClient, endpoint_name: str) -> str:
     endpoint = ManagedOnlineEndpoint(name=endpoint_name, auth_mode="key")
 
     print(f'\nCreating online endpoint {endpoint_name} and waiting for completion ...')
@@ -34,12 +38,11 @@ def create_endpoint(ml_client: MLClient):
     endpoint = long_running_operation.result()
 
     print('\t... endpoint created')
-    return endpoint, endpoint_name
+    return endpoint.scoring_uri
 
 
-def deploy_model_to_endpoint(endpoint_name: str, ml_client: MLClient):
+def deploy_model_to_endpoint(ml_client: MLClient, endpoint_name: str, deployment_name: str):
     model_name = os.getenv('MODEL_NAME')
-    deployment_name = 'blue'
 
     latest_model_version = max(
         [int(m.version) for m in ml_client.models.list(name=model_name)]
@@ -48,27 +51,27 @@ def deploy_model_to_endpoint(endpoint_name: str, ml_client: MLClient):
 
     print('\nCreating new model deployment to online endpoint and waiting for completion ...')
 
-    blue_deployment = ManagedOnlineDeployment(name=deployment_name, endpoint_name=endpoint_name,
-                                              model=model, instance_type="Standard_DS3_v2", instance_count=1)
+    deployment = ManagedOnlineDeployment(name=deployment_name, endpoint_name=endpoint_name, model=model,
+                                         instance_type="Standard_DS3_v2", instance_count=1)
 
-    long_running_operation = ml_client.online_deployments.begin_create_or_update(blue_deployment)
-    deployment = long_running_operation.result()
+    long_running_operation = ml_client.online_deployments.begin_create_or_update(deployment)
+    long_running_operation.wait()
 
     print('\t... deployment created')
-    return deployment
 
 
-def update_environment_configuration(deployment, endpoint):
-    print('\nUpdating .env-backup file')
-    dotenv.set_key('../.env-backup', 'ENDPOINT_URL', endpoint.scoring_uri)
-    dotenv.set_key('../.env-backup', 'ENDPOINT_API_KEY', '<retrieve API key using provided comands>')
+def update_environment_configuration(scoring_uri: str, endpoint_name: str, deployment_name: str):
+    print('\nUpdating environment configuration')
+    dotenv.set_key('../.env', 'ENDPOINT_URL', scoring_uri)
+    dotenv.set_key('../.env', 'ENDPOINT_API_KEY', '<retrieve API key using provided comands>')
+    dotenv.set_key('../.env', 'ENDPOINT_MODEL_DEPLOYMENT', deployment_name)
 
     print('\nExecute the following commands to retrieve the API key, then store it in the .env-backup file:')
     print('\n# Installs azure ml extension')
     print('az extension add -n ml\t\t')
     print('# Retrieve API key')
     print(f'az ml online-endpoint get-credentials -g {os.getenv("RESOURCE_GROUP")} -w {os.getenv("AML_WORKSPACE_NAME")}'
-          f' -n {deployment.endpoint_name} -o tsv --query primaryKey')
+          f' -n {endpoint_name} -o tsv --query primaryKey')
 
 
 if __name__ == '__main__':
