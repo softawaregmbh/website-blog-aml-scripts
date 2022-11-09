7832ccb88aebb09961351e869bcba4175abd2af2
Manuel Fuchs <manuel.fuchs@softaware.at>
2022-11-09 11:56:04 +0100

    Deployment: Fetch API key in script directly; Improve console messaging

--- src/3_deployment.py
+++ src/3_deployment.py
@@ -6,6 +6,8 @@ from azure.identity import DefaultAzureCredential
 from azure.ai.ml.entities import ManagedOnlineEndpoint, ManagedOnlineDeployment
 import dotenv
 
+from console import execute_cli_command, start_action, end_action
+
 dotenv.load_dotenv('../.env')
 
 
@@ -24,32 +26,33 @@ def main():
 
     deploy_model_to_endpoint(ml_client, endpoint_name, deployment_name)
 
-    # TODO: Retrieve primary key API token for authorization against endpoint
+    api_key = fetch_endpoint_api_key(endpoint_name)
 
-    update_environment_configuration(scoring_uri, endpoint_name, deployment_name)
+    update_environment_configuration(scoring_uri, deployment_name, api_key)
 
 
 def create_endpoint(ml_client: MLClient, endpoint_name: str) -> str:
-    endpoint = ManagedOnlineEndpoint(name=endpoint_name, auth_mode="key")
+    action_text = f'Create online endpoint "{endpoint_name}"'
+    start_action(action_text)
 
-    print(f'\nCreating online endpoint {endpoint_name} and waiting for completion ...')
+    endpoint = ManagedOnlineEndpoint(name=endpoint_name, auth_mode="key")
 
     long_running_operation = ml_client.online_endpoints.begin_create_or_update(endpoint)
     endpoint = long_running_operation.result()
 
-    print('\t... endpoint created')
+    end_action(action_text)
     return endpoint.scoring_uri
 
 
 def deploy_model_to_endpoint(ml_client: MLClient, endpoint_name: str, deployment_name: str):
     model_name = os.getenv('MODEL_NAME')
+    action_text = f'Deploy model "{model_name}" to endpoint'
+    start_action(action_text)
 
     latest_model_version = max(
         [int(m.version) for m in ml_client.models.list(name=model_name)]
     )
     model = ml_client.models.get(name=model_name, version=str(latest_model_version))
-
-    print('\nCreating new model deployment to online endpoint and waiting for completion ...')
 
     deployment = ManagedOnlineDeployment(name=deployment_name, endpoint_name=endpoint_name, model=model,
                                          instance_type="Standard_DS3_v2", instance_count=1)
@@ -57,21 +60,31 @@ def deploy_model_to_endpoint(ml_client: MLClient, endpoint_name: str, deployment
     long_running_operation = ml_client.online_deployments.begin_create_or_update(deployment)
     long_running_operation.wait()
 
-    print('\t... deployment created')
+    end_action(action_text)
 
 
-def update_environment_configuration(scoring_uri: str, endpoint_name: str, deployment_name: str):
-    print('\nUpdating environment configuration')
+def fetch_endpoint_api_key(endpoint_name: str) -> str:
+    action_text = f'Fetch API key of Endpoint "{endpoint_name}"'
+    start_action(action_text)
+
+    api_key = execute_cli_command(f'az ml online-endpoint get-credentials' +
+                                  f' -g {os.getenv("RESOURCE_GROUP")}' +
+                                  f' -w {os.getenv("AML_WORKSPACE_NAME")}' +
+                                  f' -n {endpoint_name}' +
+                                  f' --query "primaryKey"')
+    end_action(action_text)
+    return api_key
+
+
+def update_environment_configuration(scoring_uri: str, deployment_name: str, api_key: str):
+    action_text = 'Update environment configuration'
+    start_action(action_text)
+
     dotenv.set_key('../.env', 'ENDPOINT_URL', scoring_uri)
-    dotenv.set_key('../.env', 'ENDPOINT_API_KEY', '<retrieve API key using provided comands>')
+    dotenv.set_key('../.env', 'ENDPOINT_API_KEY', api_key)
     dotenv.set_key('../.env', 'ENDPOINT_MODEL_DEPLOYMENT', deployment_name)
 
-    print('\nExecute the following commands to retrieve the API key, then store it in the .env-backup file:')
-    print('\n# Installs azure ml extension')
-    print('az extension add -n ml\t\t')
-    print('# Retrieve API key')
-    print(f'az ml online-endpoint get-credentials -g {os.getenv("RESOURCE_GROUP")} -w {os.getenv("AML_WORKSPACE_NAME")}'
-          f' -n {endpoint_name} -o tsv --query primaryKey')
+    end_action(action_text)
 
 
 if __name__ == '__main__':
