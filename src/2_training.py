d0510dd95779edec6345b8fd0e379fd090f5cdf6
Manuel Fuchs <manuel.fuchs@softaware.at>
2022-11-09 18:35:09 +0100

    Training: Make scripts more transparent for user

--- src/2_training.py
+++ src/2_training.py
@@ -2,13 +2,15 @@ import os
 
 from azureml.core import Workspace, Dataset
 from azureml.core.authentication import ServicePrincipalAuthentication
+from dotenv import load_dotenv
 import mlflow
 import mlflow.sklearn
-from dotenv import load_dotenv
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+
+from utils import start_action, end_action
 
 load_dotenv('./.env')
 
@@ -25,22 +27,36 @@ def main():
         tenant_id=tenant_id, service_principal_id=app_id, service_principal_password=app_password
     ))
 
-    # enable debugging
     mlflow.start_run()
     mlflow.sklearn.autolog()
 
-    # fetch train and test dataset
+    x_test, x_train, y_test, y_train = fetch_and_scale_data(workspace)
+
+    digit_classifier = tune_hyperparameters(x_train, y_train)
+
+    analyze_and_export_model(digit_classifier, x_test, y_test)
+
+    mlflow.end_run()
+
+
+def fetch_and_scale_data(workspace):
+    action_text = 'Fetch and scale data'
+    start_action(action_text)
+
     train = Dataset.get_by_name(workspace, name='MNIST Database - Train Partition').to_pandas_dataframe()
     test = Dataset.get_by_name(workspace, name='MNIST Database - Test Partition').to_pandas_dataframe()
 
     x_train, x_test = train.loc[:, train.columns != 'label'], test.loc[:, test.columns != 'label']
     y_train, y_test = train[['label']].values.ravel(), test[['label']].values.ravel()
 
-    # standard scaling
     scaler = StandardScaler().fit(x_train)
     x_train, x_test = scaler.transform(x_train), scaler.transform(x_test)
 
-    # define hyperparameter grid
+    end_action(action_text)
+    return x_test, x_train, y_test, y_train
+
+
+def tune_hyperparameters(x_train, y_train):
     param_grid = {
         'hidden_layer_sizes': [(100,), (125,), (100, 100)],
         'activation': ['logistic', 'relu'],
@@ -49,13 +65,20 @@ def main():
 
         'max_iter': [500],
     }
+    action_text = 'Tune hyperparameters'
+    start_action(action_text)
 
-    # GridSearchCV
     param_tuner = GridSearchCV(MLPClassifier(), param_grid=param_grid, n_jobs=-1, cv=5, verbose=1)
     param_tuner.fit(x_train, y_train)
-    digit_classifier = param_tuner.best_estimator_
 
-    # predict on test set using best model from CV
+    end_action(action_text)
+    return param_tuner.best_estimator_
+
+
+def analyze_and_export_model(digit_classifier, x_test, y_test):
+    action_text = 'Analyze and export model'
+    start_action(action_text)
+
     y_pred = digit_classifier.predict(x_test)
     print(classification_report(y_test, y_pred))
 
@@ -65,13 +88,12 @@ def main():
         registered_model_name=model_name,
         artifact_path=model_name,
     )
-
     mlflow.sklearn.save_model(
         sk_model=digit_classifier,
         path=os.path.join(model_name, "trained_model"),
     )
 
-    mlflow.end_run()
+    end_action(action_text)
 
 
 if __name__ == '__main__':
--- src/2_training_scheduler.py
+++ src/2_training_scheduler.py
@@ -4,6 +4,8 @@ from azure.ai.ml import MLClient, command
 from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
 
+from utils import start_action, end_action
+
 load_dotenv('../.env')
 
 
@@ -15,12 +17,19 @@ def main():
         workspace_name=os.getenv('AML_WORKSPACE_NAME'),
     )
 
+    print()
+    action_text = 'Queue model training job'
+    start_action(action_text)
+
     environment = f'{os.getenv("ENVIRONMENT_NAME")}@latest'
     compute_instance = os.getenv('COMPUTE_INSTANCE_NAME')
     job = command(code='../', command='python ./src/2_training.py', environment=environment, compute=compute_instance,
                   experiment_name='train_digit_classifier_model', display_name='Digit Classifier Model Training')
 
     job = ml_client.jobs.create_or_update(job)
+    end_action(action_text)
+    print()
+
     ml_client.jobs.stream(name=job.name)
 
 
